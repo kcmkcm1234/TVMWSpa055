88faaba2d165012c121c7b864b15065737a163cf
suvaneeth <suvaneeth@gmail.com>
2017-06-20 10:09:30 +0300

    settings

--- UserInterface/Global.asax.cs
+++ UserInterface/Global.asax.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
 using System.Web.Optimization;
@@ -13,7 +14,11 @@ namespace UserInterface
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+
+          //  WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            MappingConfig.RegisterMaps();
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
