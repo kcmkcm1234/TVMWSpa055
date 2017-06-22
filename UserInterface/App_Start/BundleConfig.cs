cb2b621e3490a6d6810fcf409293dbd0fbb20161
suvaneeth <suvaneeth@gmail.com>
2017-06-22 16:23:53 +0300

    design

--- UserInterface/App_Start/BundleConfig.cs
+++ UserInterface/App_Start/BundleConfig.cs
@@ -14,6 +14,8 @@ namespace UserInterface.App_Start
             bundles.Add(new StyleBundle("~/Content/DataTables/css/datatableButtons").Include("~/Content/DataTables/css/buttons.dataTables.min.css"));
             bundles.Add(new StyleBundle("~/Content/DataTables/css/datatableFixedColumns").Include("~/Content/DataTables/css/fixedColumns.dataTables.min.css"));
 
+            bundles.Add(new StyleBundle("~/AdminLTE/bootstrap/css/bootstrap").Include("~/AdminLTE/bootstrap/css/bootstrap.min.css", "~/AdminLTE/plugins/jvectormap/jquery-jvectormap-1.2.2.css", "~/AdminLTE/dist/css/AdminLTE.min.css", "~/AdminLTE/dist/css/skins/_all-skins.min.css"));
+
             //-------------------
             bundles.Add(new StyleBundle("~/Content/UserCSS/Login").Include("~/Content/UserCSS/Login.css"));
 
@@ -32,6 +34,8 @@ namespace UserInterface.App_Start
             bundles.Add(new ScriptBundle("~/bundles/userpluginjs").Include("~/Scripts/jquery.noty.packaged.min.js", "~/Scripts/custom.js", "~/Scripts/Chart.js", "~/Scripts/sweetalert.min.js"));
             bundles.Add(new ScriptBundle("~/bundles/bootstrapdatepicker").Include("~/Scripts/bootstrap-datepicker.min.js"));
             bundles.Add(new ScriptBundle("~/bundles/userpluginjs").Include("~/Scripts/jquery.noty.packaged.min.js", "~/Scripts/custom.js", "~/Scripts/Chart.js", "~/Scripts/sweetalert.min.js"));
+
+            bundles.Add(new ScriptBundle("~/AdminLTE/plugins").Include("~/AdminLTE/plugins/jQuery/jquery-3.1.1.min.js", "~/AdminLTE/bootstrap/js/bootstrap.min.js", "~/AdminLTE/plugins/fastclick/fastclick.js", "~/AdminLTE/dist/js/adminlte.js", "~/AdminLTE/plugins/sparkline/jquery.sparkline.min.js", "~/AdminLTE/plugins/slimScroll/jquery.slimscroll.min.js", "~/AdminLTE/plugins/chartjs/Chart.min.js", "~/AdminLTE/dist/js/pages/dashboard2.js"));
             //----------------------
             bundles.Add(new ScriptBundle("~/bundles/Login").Include("~/Scripts/UserJS/Login.js"));
 
