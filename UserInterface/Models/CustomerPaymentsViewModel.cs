0d2a1d92280c9a1fad67516067d99102352f0361
tvm-gibin <gibin.jacob@thrithvam.me>
2017-07-04 17:18:19 +0530

    CustomerPaymentsdesign

--- UserInterface/Models/CustomerPaymentsViewModel.cs
+++ UserInterface/Models/CustomerPaymentsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,50 @@ namespace UserInterface.Models
 {
     public class CustomerPaymentsViewModel
     {
+
+        public Guid ID { get; set; }
+        public string EntryNo { get; set; }
+
+        [Display(Name = "Received To Company")]
+        public string RecdToComanyCode { get; set; }
+
+        [Display(Name = "Customer Name")]
+        public Guid CustomerID { get; set; }
+
+        [Display(Name = "Payment Mode")]
+        public string PaymentMode { get; set; }
+
+        public Guid DepWithdID { get; set; }
+
+        [Display(Name = "Deposit To")]
+        public string BankCode { get; set; }
+
+        [Display(Name = "Reference No.")]
+        public string PaymentRef { get; set; }
+
+        [Display(Name = "Payment Date")]
+        public DateTime PaymentDate { get; set; }
+
+        [Display(Name = "General Notes")]
+        public string GeneralNotes { get; set; }
+
+        public decimal TotalRecdAmt { get; set; }
+
+        public string PaymentDateFormatted { get; set; }
+        public List<CustomerPaymentsDetailViewModel> CustomerPaymentsDetail { get; set; }
+
+    }
+    public class CustomerPaymentsDetailViewModel
+    {
+        public Guid ID { get; set; }
+        public Guid PaymentID { get; set; }
+        public Guid InvoiceID { get; set; }
+        public DateTime PaymentDueDate { get; set; }
+        public decimal OriginalInvoiceAmount { get; set; }
+        public decimal BalanceDue { get; set; }
+        public decimal PaidAmount { get; set; }
+
+        public string PaymentDueDateFormatted { get; set; }
+
     }
 }
