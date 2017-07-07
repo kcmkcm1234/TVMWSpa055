e2e54ee691bee8b47fb942e9249d8efe51f9aea9
tvm-anija <anija.g@thrithvam.me>
2017-07-07 12:05:32 +0530

    updated Bank

--- UserInterface/Models/BankViewModel.cs
+++ UserInterface/Models/BankViewModel.cs
@@ -12,13 +12,16 @@ namespace UserInterface.Models
         [Display(Name = "Code")]
         [MaxLength(5)]
         [Required(ErrorMessage ="Code is required")]
+        [RegularExpression(@"^[a-zA-Z0-9_]*$", ErrorMessage = "Entered code is not valid.")]
         public string Code { get; set; }
         public string hdnCode { get; set; }
         [Display(Name = "Name")]
         [MaxLength(100)]
+        [RegularExpression(@"^[A-Za-z0-9? ,_-]+$", ErrorMessage = "Entered name is not valid.")]
         [Required(ErrorMessage = "Company Name")]
         public string Name { get; set; }
-        [Display(Name = "Company")]       
+        [Display(Name = "Company")]
+        [MaxLength(10)]
         public string CompanyCode { get; set; }
         public CommonViewModel commonObj { get; set; }
         public List<SelectListItem> BanksList { get; set; }
