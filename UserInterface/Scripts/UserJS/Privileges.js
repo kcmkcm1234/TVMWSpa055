5a142a5d1f4ad4698f86da8251dffed8fbd1f0d2
tvm-albert <albertthomson@thrithvam.ae>
2017-07-15 10:28:40 +0530

    date column filling done SAM

--- UserInterface/Scripts/UserJS/Privileges.js
+++ UserInterface/Scripts/UserJS/Privileges.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
                { "data": "RoleName" },
                { "data": "ModuleName", "defaultContent": "<i>-</i>" },
                { "data": "AccessDescription", "defaultContent": "<i>-</i>" },
-               { "data": "CreatedDate", "defaultContent": "<i>-</i>" },
+               { "data": "commonDetails.CreatedDatestr", "defaultContent": "<i>-</i>" },
                { "data": null, "orderable": false, "defaultContent": '<a href="#" onclick="Edit(this)"<i class="glyphicon glyphicon-share-alt" aria-hidden="true"></i></a>' }
              ],
              columnDefs: [{ "targets": [0], "visible": false, "searchable": false },
