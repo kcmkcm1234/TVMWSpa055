9c60b1a5a9eb4154daf3502a9f9e4a10ff11e532
tvm-praveena <praveena.thrithvam@outlook.com>
2017-10-05 18:57:06 +0530

    Added change with the data model find missing

--- SPAccounts.DataAccessObject/DTO/Reports.cs
+++ SPAccounts.DataAccessObject/DTO/Reports.cs
@@ -291,4 +291,39 @@ namespace SPAccounts.DataAccessObject.DTO
         public decimal Balance { get; set; }
         public string SupplierCode { get; set; }
     }
+    public class OtherIncomeSummaryReport
+    {
+        public string AccountHeadORSubtype { get; set; }
+        public decimal Amount { get; set; }
+        public string OriginCompany { get; set; }
+        public string Search { get; set; }
+        public string AccountHead { get; set; }
+    }
+
+    public class OtherIncomeDetailsReport
+    {
+        public string AccountHeadORSubtype { get; set; }
+        public string AccountHead { get; set; }
+        public string PaymentMode { get; set; }
+        public string PaymentReference { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public string Company { get; set; }
+        public string OriginCompany { get; set; }
+        public string Date { get; set; }
+    }
+
+    public class DailyLedgerReport
+    {
+        public string TransactionDate { get; set; }
+        public string EntryType { get; set; }
+        public string MainHead { get; set; }
+        public string AccountHead { get; set; }
+        public string ReferenceNo { get; set; }
+        public string CustomerORemployee { get; set; }
+        public string Debit { get; set; }
+        public string Credit { get; set; }
+        public string PayMode { get; set; }
+        public string Remarks { get; set; }
+    }
 }
