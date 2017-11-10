ffbafc37903360f9f9668278bfb7ea9f3fce6b51
tvm-gibin <gibin.jacob@thrithvam.me>
2017-11-10 12:58:53 +0530

    Deposit and withdrawal bounce and delete

--- SPAccounts.BusinessService/Contracts/IDepositAndWithdrawalsBusiness.cs
+++ SPAccounts.BusinessService/Contracts/IDepositAndWithdrawalsBusiness.cs
@@ -13,6 +13,7 @@ namespace SPAccounts.BusinessService.Contracts
         DepositAndWithdrawals GetDepositAndWithdrawalDetails(Guid ID);
         DepositAndWithdrawals GetTransferCashById(Guid TransferId);
         object InsertUpdateDepositAndWithdrawals(DepositAndWithdrawals _depositAndWithdrawalsObj);
+        object DeleteDepositandwithdrawal(Guid ID, string UserName);
         object ClearCheque(string IDS,string date);
         List<DepositAndWithdrawals> GetUndepositedCheque(string FromDate, string ToDate);
         string GetUndepositedChequeCount(string Date);
