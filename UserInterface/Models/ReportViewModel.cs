077a28aa37d6c967dd69a423e75f39156ca88bc3
Angel Paul <Angel Paul@thrithvam>
2018-01-20 16:06:57 +0530

    Customer recivable ageing details Report

--- UserInterface/Models/ReportViewModel.cs
+++ UserInterface/Models/ReportViewModel.cs
@@ -641,6 +641,12 @@ namespace UserInterface.Models
         public string CompanyCode { get; set; }      
         public string SupplierIDs { get; set; }
         public string Search { get; set; }
-    }  
-  
+    }
+    public class ReportAccountsReceivableAgeingSearchViewModel
+    {
+        public string FromDate { get; set; }
+        public string ToDate { get; set; }
+        public string CompanyCode { get; set; }
+        public string[] Customerids { get; set; }
+    }
 }
